58cfef7b705c07f24fbd6868ec308180d9d96b19
NiAlKo <35404879+NiAlKo@users.noreply.github.com>
2022-01-17 16:16:56 +0300

    added section slider

--- js/script.js
+++ js/script.js
@@ -1,3 +1,45 @@
+
+
+$(document).ready(function() {
+	// Burger menu
+	$('.icon-menu').click(function(evt) {
+		$('.icon-menu ,.menu__body').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+// Adaptive img
+function ibg(){
+	$.each($('.ibg'), function(index, val) {
+	if($(this).find('img').length>0){
+	$(this).css('background-image','url("'+$(this).find('img').attr('src')+'")');
+	}
+	});
+	}
+
+	ibg();
+	// Slider
+	if ($('.slider__body').length>0){
+		$('.slider__body').slick({
+			// autoplay: true,
+			// infinite: false,
+			dots: true,
+			arrows: false,
+			accessibility: false,
+			slidesToShow:1,
+			autoplaySpeed: 3000,
+			adaptiveHeight: true,
+			nextArrow:'<button type="button" class="slick-next"></button>',
+			prevArrow:'<button type="button" class="slick-prev"></button>',
+			responsive:[{
+					breakpoint: 768,
+					settings: {}
+				}]	
+		});
+	}
+});
+
+
+
+/*//=================-vanill_js-===================
 // Burger menu
 let burger = document.querySelector(".icon-menu");
 let menu = document.querySelector(".menu__body");
@@ -18,3 +60,4 @@ function ibg() {
 	}
 }
 ibg();
+*/
